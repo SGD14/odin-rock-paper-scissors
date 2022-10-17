902d2ac9f84b6caef12fcf2e51ef585e18a875fb
Dani <24252795+SGD14@users.noreply.github.com>
2022-10-17 14:12:51 +0200

    Add player buttons and score counter

--- javascript.js
+++ javascript.js
@@ -11,6 +11,7 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
+    console.log("PLAY " + playerSelection);
     if(playerSelection.toLowerCase() === computerSelection.toLowerCase())
         return 0;
 
@@ -27,3 +28,34 @@ function playRound(playerSelection, computerSelection) {
 function isValidSelection(selection) {
     return selection.toLowerCase() === "rock" || selection.toLowerCase() === "paper" || selection.toLowerCase() === "scissors";
 }
+
+let playerScore = 0;
+let computerScore = 0;
+
+let rockButton = document.querySelector("#rock");
+let paperButton = document.querySelector("#paper");
+let scissorsButton = document.querySelector("#scissors");
+let scoreContainer = document.querySelector("#score-container");
+
+const playFunction = (playerSelection) => {
+    let computerChoice = getComputerChoice();
+
+    let roundResult = playRound(playerSelection, computerChoice);
+
+    switch(roundResult){
+        case 1: playerScore++; break;
+        case 2: computerScore++; break;
+    }
+
+    scoreContainer.textContent = "Player Score: " + playerScore + " | Computer Score: " + computerScore;
+
+    if(playerScore >= 5)
+        scoreContainer.textContent = scoreContainer.textContent + " - Player wins!";
+
+    if(computerScore >=5)
+        scoreContainer.textContent = scoreContainer.textContent + " - Computer wins!";
+}
+
+rockButton.addEventListener('click', () => playFunction("Rock"));
+paperButton.addEventListener('click', () => playFunction("Paper"));
+scissorsButton.addEventListener('click', () => playFunction("Scissors"));
